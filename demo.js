aa873010210ce2cfb4ccd6adbb36e26f83201d7f
rupalikarmalkar <rupalirk87@gmail.com>
2022-10-04 15:54:48 +0530

    first program in java script
    first program in java script

---
+++ demo.js
@@ -0,0 +1,8 @@
+// this is my first program in java script
+console.log("Hello")
+console.log("My name is Rupali")
+console.info("This is info")
+// this is showing warning
+console.warn("this is warning")
+// it will show error
+console.error("opps error")
---
+++ jsAssign.js
@@ -0,0 +1,23 @@
+console.log("Rupali Ravindra karmalkar")
+console.log("My Hobbies are:")
+console.log("Cooking")
+console.log("Music")
+console.log("Explore new things")
+// My personal info
+console.info('My Personal strength are:')
+console.log("self-confidence")
+console.log("creative")
+console.log("Honest")
+// my week areas
+console.warn("My three weakness:")
+console.warn("over thinker")
+console.warn("shorttemper")
+console.warn("Emotional")
+
+console.log(30+50)
+console.log(49*479)
+
+
+
+
+
