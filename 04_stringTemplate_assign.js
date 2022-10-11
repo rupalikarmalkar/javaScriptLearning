fa1a7ae1eca36a5b3b72942f4c01226c59e7a514
rupalikarmalkar <rupalirk87@gmail.com>
2022-10-11 14:29:41 +0530

    String methods and Assignments
    String methods and Assignments

---
+++ 04_stringTemplate_assign.js
@@ -0,0 +1,50 @@
+
+var str_fun_var = function () {
+    var string_var = "    Hey you are doing good,keep it up    ";
+
+    console.log(`The string is= ${string_var}`);
+    var str_lenght = string_var.length;
+    console.log("-------------------------------------------------------");
+
+    console.log(`The lenght of given string is ${str_lenght} chatecrers.`);
+    console.log("-------------------------------------------------------");
+
+    console.log("Before trim the string is=", string_var);
+    var srt_trim = string_var.trim();
+    console.log("After trim the string is=", srt_trim);
+    console.log("-------------------------------------------------------");
+
+    console.log(`The lenght of given string is ${str_lenght} chatecrers.`);
+
+    var trimed_var = srt_trim.length;
+
+    console.log(`Trimed lenght of given string is ${trimed_var} charecters.`);
+    var extra_space = console.log("Extra space count is=", str_lenght - trimed_var, "charectes.");
+    console.log("-------------------------------------------------------");
+
+    var first_char = srt_trim.charAt(0);
+    var last_char = srt_trim.charAt(trimed_var - 1)
+    console.log(`First charecter=${first_char} Last charecter=${last_char}`);
+
+    console.log("-------------------------------------------------------");
+
+    var index_of_good = string_var.indexOf("good");
+    console.log(`The index of word "good" is ${index_of_good}.`);
+
+    console.log("-------------------------------------------------------");
+
+    var sub_str = string_var.substring(22);
+    var slice_str = string_var.slice(22);
+    console.log(`Output using substring is= ${sub_str}& using slice is= ${slice_str}`);
+    console.log("-------------------------------------------------------");
+    var end_with = string_var.endsWith("up");
+    console.log(`The given string is ending with "up" is ${end_with}.`);
+
+    console.log("-------------------------------------------------------");
+
+    var start_str_with = srt_trim.startsWith("Hey");
+    console.log(`The trimed string is start with "Hey" is ${start_str_with}. `);
+
+    console.log("-------------------------------------------------------");
+}
+str_fun_var();
---
+++ 04_string_temp_ass2.js
@@ -0,0 +1,8 @@
+console.log("My dream Company is TCS.");
+console.log("My hobbies are:");
+console.log("               1.Learn new things");
+console.log("               2.Cooking");
+console.log("               3.Music");
+console.log( "My hobbies are:" , "1.Learn new things" +" 2.Cooking " + " 3.Music");
+
+
