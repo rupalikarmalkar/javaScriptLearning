4c8e5af2e5aa79e8f50583b86b0ff2baed8441cf
rupalikarmalkar <rupalirk87@gmail.com>
2022-10-14 14:25:04 +0530

    if and for statements assignments
    if and for statements assignments

---
+++ 06_switch_case.js
@@ -0,0 +1,32 @@
+var dayNumber=2;
+switch (dayNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+        case 2:
+        console.log("Tuesday");
+            break;
+            case 3 :
+        console.log("Wednesdsy");
+            break;
+
+            case 4:
+        console.log("Tharsday");
+            break;
+
+            case 5:
+        console.log("Friday");
+            break;
+
+            case 6:
+        console.log("Saturday");
+            break;
+
+            case 7:
+        console.log("Sunday");
+            break;
+
+
+    default:
+        break;
+}
---
+++ 06_while_doWhile_assign1.js
@@ -0,0 +1,33 @@
+for (let for_5_15 = 1; for_5_15 <= 15; for_5_15++) {
+    console.log(for_5_15);
+    
+}
+console.log("---------------------------------------------");
+
+for (let for_40_50_dec = 50; for_40_50_dec>=40 ; for_40_50_dec--) {
+    console.log(for_40_50_dec);
+    
+}
+console.log("---------------------------------------------");
+
+for (let for_first_15_odd = 1; for_first_15_odd <=30;for_first_15_odd =for_first_15_odd+2) {
+    console.log(for_first_15_odd); 
+    
+}
+console.log("---------------------------------------------");
+
+for (let for_5_table = 5; for_5_table <=50;  for_5_table=for_5_table+5) {
+    console.log(for_5_table);
+    
+}
+console.log("---------------------------------------------");
+
+for (let for_10s_table=10; for_10s_table<=100;  for_10s_table=for_10s_table+10) {
+     console.log(for_10s_table);
+}
+console.log("---------------------------------------------");
+
+for (let for_10_rev_table = 100; for_10_rev_table >= 10; for_10_rev_table=for_10_rev_table-10) {
+    console.log(for_10_rev_table);
+    
+}
